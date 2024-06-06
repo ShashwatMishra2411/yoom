8c9325c6d9692991789985ccab9567a72dc453a9
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-06-07 02:36:52 +0530

    feat: Add Sidebar component
    The code changes add a new file `Sidebar.jsx` which contains the `Sidebar` component. This component is responsible for rendering the sidebar section of the application, providing navigation links and highlighting the active link.

---
+++ components/Sidebar.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { sidebarLinks } from "@constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import cn from "classnames";
+
+export default function Sidebar() {
+  const pathName = usePathname();
+  return (
+    <section className="sticky left-0 top-0 flex min-h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathName === link.route || pathName.startsWith(link.route);
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              // cn is used for dynamic classnames
+              className={cn(
+                "flex gap-4 itmes-center p-4 rounded-lg justify-start",
+                {
+                  "bg-blue-1": isActive,
+                }
+              )}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
