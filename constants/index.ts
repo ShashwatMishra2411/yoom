d42e3394797f775cb624055d2ac7952e791e0fa2
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-06-07 02:37:11 +0530

    feat: Add sidebar links to constants file
    The code changes in the `constants/index.ts` file add an array of sidebar links to the application. Each link object contains an image URL, route, and label. This improves the navigation functionality by providing predefined links for the sidebar.

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        imgUrl: '/images/icons/Home.svg',
+        route:'/',
+        label: 'Home'
+    },
+    {
+        imgUrl: '/images/icons/upcoming.svg',
+        route:'/upcoming',
+        label: 'Upcoming'
+    },
+    {
+        imgUrl: '/images/icons/previous.svg',
+        route:'/previous',
+        label: 'Previous'
+    },
+    {
+        imgUrl: '/images/icons/Video.svg',
+        route:'/recordings',
+        label: 'Recordings'
+    },
+    {
+        imgUrl: '/images/icons/add-personal.svg',
+        route:'/personal-room',
+        label: 'Personal Room'
+    }
+]
