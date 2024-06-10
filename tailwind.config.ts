8381816f3577ecf0e19f612574b70588c35d3c7a
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-06-10 23:12:18 +0530

    💄 chore: Update lazy loading configuration for images in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from 'tailwindcss';
 const config = {
   darkMode: ['class'],
   content: [
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    './app/**/*.{js,jsx,ts,tsx}',
-    './src/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{jsx, ts,tsx}',
+    './components/**/*.{jsx, ts,tsx}',
+    './app/**/*.{jsx, ts,tsx}',
+    './src/**/*.{jsx, ts,tsx}',
   ],
   prefix: '',
   theme: {
