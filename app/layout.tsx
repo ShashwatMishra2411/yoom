fcc68416396c472c26e1bcb2c5b683ed3452eb0d
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-06-13 22:47:02 +0530

    ```text feat: Add Toaster component for displaying toast messages
    This commit adds a new component called Toaster, which is used to display toast messages in the application. The Toaster component is imported from the "@/components/ui/toaster" module and is added to the body of the RootLayout component. This feature enhances the user experience by providing a convenient way to show informative messages to the user.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,9 @@ export default function RootLayout({
   return (
     <ClerkProvider
       appearance={{
-        layout:{
-          logoImageUrl:"/icons/yoom-logo.svg",
-          socialButtonsVariant:"iconButton",
+        layout: {
+          logoImageUrl: "/icons/yoom-logo.svg",
+          socialButtonsVariant: "iconButton",
         },
         variables: {
           colorText: "#fff",
@@ -31,7 +32,10 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+          <Toaster />
+        </body>
       </html>
     </ClerkProvider>
   );
