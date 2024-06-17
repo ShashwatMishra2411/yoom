d0ebd96fb9352958047056581170ae37dafed485
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-06-18 03:11:59 +0530

    feat: Add TooltipProvider to RootLayout
    This commit adds the TooltipProvider component to the RootLayout in the layout.jsx file. The TooltipProvider wraps the existing StreamClientProvider, allowing tooltips to be displayed throughout the application. This feature enhances the user experience by providing additional information and context when hovering over certain elements.

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { StreamClientProvider } from "@Providers/StreamClientProvider";
-
+import { TooltipProvider } from "@components/ui/tooltip";
+export const metadata = {
+  title: "Yoom",
+  description: "Real Time Video Meeting Application",
+  icons:{
+    icon:"/icons/logo.svg"
+  }
+};
 export default function RootLayout({ children }) {
   return (
     <main>
-      <StreamClientProvider> {children}</StreamClientProvider>
+      <StreamClientProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+      </StreamClientProvider>
     </main>
   );
 }
